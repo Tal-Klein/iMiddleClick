396e0930c1e5b1013c65f17aad850873cc631e08
Tal Klein <talklein17@gmail.com>
2026-08-11 01:07:25 +0300

    Initial Commit

---
+++ demo_middleClickForMac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  demo_middleClickForMac
+//
+//  Created by Tal Klein on 11/08/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ demo_middleClickForMac/demo_middleClickForMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  demo_middleClickForMacApp.swift
+//  demo_middleClickForMac
+//
+//  Created by Tal Klein on 11/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct demo_middleClickForMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
